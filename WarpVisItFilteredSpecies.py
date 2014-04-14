f8e57176fbd9e571ad0ce28fb7ab5a5e5543fa36
Oliver Ruebel <oruebel@lbl.gov>
2014-04-14 11:58:11 -0700

    Modified the WarpVisItFilteredSpecies as follows:  -- Add **kwargs to __init__ to allow custom parameters for filters  -- Added function to Set the filter function and filter parameters  -- Updated the interface of filter function to have species as     required parameter and to optionally support self as first     parameter to give the filter access to the filtered species opject  -- Renamed __Filter to __FilterFunc

--- WarpVisItFilteredSpecies.py
+++ WarpVisItFilteredSpecies.py
@@ -1,5 +1,7 @@
 import numpy
 import warp
+import inspect
+import types
 
 #############################################################################
 class WarpVisItFilteredSpecies(object):
@@ -10,36 +12,102 @@ class WarpVisItFilteredSpecies(object):
     declared like: def filterFunc(x,y,z,ux,uy,uz): ... return ids
     """
     #------------------------------------------------------------------------
-    def __init__(self,species,filterFunc,filterName):
-        """ """
+    def __init__(self,species,filterFunc,filterName,**kwargs):
+        """
+        Initalize the WarpVisItFilteredSpecies object:
+        
+        Parameters: 
+        
+            species : The species object to which filtering is applied
+            filterFunc : The filter function to be applied
+            filterName : The name of the filter
+            kwargs : Additional parameters to be passed to the filter function
+            
+        Attributes:
+        
+            __Species : The species object to which the filter is applied to
+            __FilterFunc : The filter funtion
+            __FilterArgs : Additional parameters for the filter function
+            __IDs : List of selected ids to avoid repeated evalutation of the filter.
+            __Key : Timestep key when the filter was last evaluated. Used to check __Ids.
+            name : Name for the filtered species, defined by filterName and species.name
+            type : Type of the species defined by species.type.
+            
+        
+        """
+        # Check if the filterfunction is valid
+        if 'species' not in inspect.getargspec(filterFunc).args:
+            raise KeyError("filterfunc must have species as input argument.")
+        #Set __FilterFunc and __FilterArgs using the approbirate set functions
+        self.SetFilter(filterFunc)
+        self.SetFilterArgs(**kwargs)
+        # Define other object variables
         self.__Species = species
-        self.__Filter = filterFunc
         self.__Ids = None
         self.__Key = -1
         self.name = species.name + filterName
         self.type = species.type
+        
         return
 
     #------------------------------------------------------------------------
-    def Update(self, key):
+    def Update(self, key):                                      
         """
-        Generate the ids, by calling user provided function. Does
-        something only if key has changed since the last invokation.
+        Generate the ids, by calling the user provided filter function. Ids are
+        only updated if the key has changed or Ids are not available.
+        
+        Parameters:
+        
+            key : Timestep key used to check whether the existing IDs are valid.
+             
         """
         if (self.__Ids is None) or (self.__Key != key):
-            self.__Ids = self.__Filter(self.__Species.getx(gather=0),
-                    self.__Species.gety(gather=0),
-                    self.__Species.getz(gather=0),
-                    self.__Species.getux(gather=0),
-                    self.__Species.getuy(gather=0),
-                    self.__Species.getuz(gather=0))
+            self.__Ids = self.__FilterFunc(self.__Species, **self.__FilterArgs)
             self.__Key = key
         return
+
+    
+    #------------------------------------------------------------------------
+    def SetFilter(self, filterFunc):         
+        """
+        Change the filter function used.
+        
+        Parameters:
+        
+            filterFunc : The new filter function to be used.
+        
+        """
+        # Determine whether we need to bind the filter function to the object
+        if inspect.getargspec(filterFunc).args[0] == 'self':
+            self.__FilterFunc = types.MethodType(filterFunc, self)
+        else:
+            self.__FilterFunc = filterFunc
+        self.__Ids = None
+    
+    #------------------------------------------------------------------------
+    def SetFilterArgs(self, **kwargs):                         
+        """
+        Change filter parameters
+        
+        Parameters:
+        
+            **kwargs: Define any keyword parameters used by the filter function
+        
+        """
+        self.__FilterArgs = kwargs
+        self.__Ids = None
 
     #------------------------------------------------------------------------
     def Filter(self, data):
         """
-        Return filtered data
+        Return filtered data. Filter the given data array. Use the provided 
+        get functions to access common variables associated with the species,
+        e.g, getx, gety, getz etc. 
+        
+        Parameters:
+        
+            data : The data array to be filtered
+             
         """
         self.Update(warp.warp.top.it)
         if (len(self.__Ids) > 0):
@@ -83,3 +151,9 @@ class WarpVisItFilteredSpecies(object):
     def getweights(self,**kwargs): return self.Filter(self.__Species.getweights(**kwargs))
     def getke(self,**kwargs): return self.Filter(self.__Species.getke(**kwargs))
     def getrank(self,**kwargs): return self.Filter(self.__Species.getrank(**kwargs))
+
+
+
+
+
+
